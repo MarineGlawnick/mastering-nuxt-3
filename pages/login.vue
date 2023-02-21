3281953dd8adb7b040192281612863112dcfc6b8
Marine Glawnick <mglawnick@seela.io>
2023-02-21 15:07:25 +0100

    4-6-logging-in-with-github

--- pages/login.vue
+++ pages/login.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="prose w-full max-w-2xl h-9">
     <h1>Log in to {{ title }}</h1>
-    <button class="bg-blue-500 text-white font-bold py-2 px-4 rounded">
+    <button class="bg-blue-500 text-white font-bold py-2 px-4 rounded" @click="login">
       Log in with Github
     </button>
   </div>
@@ -9,4 +9,13 @@
 
 <script lang="ts" setup>
 const {title} = useCourse();
+const supabase = useSupabaseClient();
+const login = async () => {
+  const {error} = await supabase.auth.signInWithOAuth({
+    provider: 'github',
+  });
+  if (error) {
+    console.error(error);
+  }
+};
 </script>
